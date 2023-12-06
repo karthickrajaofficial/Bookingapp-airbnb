9d2d5bf8eaac5129c04681c1bacb69cc2a75c91e
Karthickraja <113756047+karthickrajaofficial@users.noreply.github.com>
2023-12-06 22:45:05 +0530

    Update Image.jsx

--- client/src/Image.jsx
+++ client/src/Image.jsx
@@ -1,5 +1,5 @@
 export default function Image({ src, ...rest }) {
-    const baseUrl =  'http://localhost:4000/';
+    const baseUrl =  'https://karthick-bookingapp-airbnb.vercel.app/api';
     const imageUrl = src && src.includes('http') ? src : baseUrl + 'uploads/' + src;
   
     return <img {...rest} src={imageUrl} alt={''} />;
