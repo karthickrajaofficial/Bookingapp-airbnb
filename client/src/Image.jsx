746499fdaa098f7abdd13c342da3226daa2d6cb4
Karthickraja <113756047+karthickrajaofficial@users.noreply.github.com>
2023-12-08 12:21:17 +0530

    Update Image.jsx s3

--- client/src/Image.jsx
+++ client/src/Image.jsx
@@ -1,8 +1,13 @@
-export default function Image({src,...rest}) {
-  src = src && src.includes('https://')
-    ? src
-    : 'http://localhost:4000/uploads/'+src;
-  return (
-    <img {...rest} src={src} alt={''} />
-  );
+export default function Image({ src, ...rest }) {
+  let imageUrl;
+
+  if (src && src.startsWith('s3://')) {
+    // Handle S3 URL
+    imageUrl = src.replace('s3://', 'https://your-s3-bucket-name.s3.amazonaws.com/');
+  } else {
+    // Assuming it's a relative path
+    imageUrl = 'http://localhost:4000/uploads/' + src;
+  }
+
+  return <img {...rest} src={imageUrl} alt={''} />;
 }
